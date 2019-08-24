b95a9ea0757a14b4fe28c08ad306d2c8f782faab
Ken Huang <hunm4dj0@gmail.com>
2019-08-24 12:04:18 +0800

    basic version done

---
+++ password_retry.py
@@ -0,0 +1,13 @@
+# 密碼重試程式
+password = 'a123456'
+x = 3
+while x > 0: #while True
+	pwd = input('請輸入密碼: ')
+	if pwd != password:
+		x = x - 1
+		print('密碼錯誤! 還有%x次機會 '%x)
+	else:
+		print('登入成功!')
+		break
+if x == 0:
+	print('登入失敗!')
